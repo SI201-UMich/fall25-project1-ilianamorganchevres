b916e905ee6d3e9959f33f19c486fbc6c485d3b9
Iliana <ilianamorgan7@gmail.com>
2025-10-15 12:00:28 -0400

    started creating the profit_summary function

--- Project_1.py
+++ Project_1.py
@@ -4,17 +4,24 @@
 #Project 1 
 def csv_import(file):
     import csv
-    with open('file', 'r') as f_name: 
+    with open(file, 'r') as f_name: 
         csv_reader = csv.reader(f_name)
-    
-    for row in csv_reader:
-        print(row)
-
+        for row in csv_reader:
+            print(row)
+    #file.close()
+#make this into a nested dict 
 csv_import('SampleSuperstore.csv')
 
 #profit summary dictionary function - makes a nested 
 def profit_summary(csv_dict):
-    pass 
+    profit_dict = []
+    for key,value in csv_dict.items():
+        region = value[6] 
+        category = value[1]
+        profit_dict[region] = x
+        profit_dict[category] = x
+    return profit_dict 
+    
 #dictionary where teh keys are regions and categories and values are profits 
 #average profit for each category in a dictionary 
 def avg_profit(csv_dict):
@@ -26,3 +33,7 @@ def top_performers(csv_dict):
 #Percentage of each category by region
 def percent_byregion(csv_dict):
     pass 
+
+#have to make the main function 
+main()
+pass 
