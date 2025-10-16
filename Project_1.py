6be3e6afbc5c1503b2f128c60f9f33c93cedf553
Iliana <ilianamorgan7@gmail.com>
2025-10-16 12:26:05 -0400

    Tried to fix the avg_profit function

--- Project_1.py
+++ Project_1.py
@@ -11,7 +11,7 @@ def csv_import(file):
         count = 0 
         for row in csv_reader:
             count += 1 
-            print(row)
+            #print(row)
             csv_dict[count] = row 
     return csv_dict
 #make this into a nested dict 
@@ -23,14 +23,27 @@ def profit_summary(csv_dict):
     for key,value in csv_dict.items():
         region = value[6] 
         category = value[1]
-        profit_dict[region] = x
-        profit_dict[category] = x
+        profit_dict[key[6]] = region
+        profit_dict[key[1]] = category
     return profit_dict 
     
 #dictionary where the keys are regions and categories and values are profits 
-#average profit for each category in a dictionary 
+#average profit for each category in a list containing dicts 
 def avg_profit(csv_dict):
-    pass 
+    avg_profit = []
+    for key, value in csv_dict.items():
+        print(key, value)
+        count = 0 
+        profit = 0 
+        region = value[6]
+        category = value[7]
+        avg = profit / count 
+        if key == 'Region':
+            avg_profit.append[{f"Region: {region}, Average Profit: {avg}"}]
+        if key == "Category":
+            avg_profit.append[{f"Category: {category}, Average Profit: {avg}"}]
+    return avg_profit
+
 #top performers 
 def top_performers(csv_dict):
     pass 
